f1869500a12ed592c737f12510711d96becbfd65
Steven Havko <stevenhavko@gmail.com>
2015-11-04 16:15:14 -0500

    Can now pass multiple players

--- test.py
+++ test.py
@@ -5,30 +5,22 @@ import nflgame
 form = cgi.FieldStorage() 
 
 # Get data from fields
-def calcFant(player):
-	fpoints = 0
-	tdpoints = 0
-	
-	if player.guess_position == "QB":
-		tdpoints = player.tds * 4
-	else:
-		tdpoints = player.tds * 6
-		
-	fpoints = tdpoints + (player.rushing_yds/10.0) + (player.receiving_yds/10.0) + (player.passing_yds/25.0) + (player_stats.twoptm * 2) - (player_stats.passing_ints * 2) - (player_stats.fumbles_lost * 2)
-	return fpoints
 
+playersArr = []
 player_name = form.getvalue('player_name')
+playersArr = player_name.split(',')
 games = nflgame.games(2015, week=1)
 players = nflgame.combine(games)
 player_stats = players.name(player_name)
-playerPoints = calcFant(player_stats)
+
 
 #players = nflgame.combine_game_stats(games)
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
 print "<link rel=%s type=%s href=%s>" % ("stylesheet", "text/css", "/projects/cgitest/styles.css")
-print "<title>Hello - Second CGI Program</title>"
+print "<script type=%s src=%s></script>" % ("text/javascript", "/projects/cgitest/playertracker.js")
+print "<title>Player Tracker</title>"
 print "</head>"
 print "<body>"
 print """
@@ -38,9 +30,34 @@ print """
 
 	<input type="submit" value="Submit" />
 	</form>
+	
 	"""
-print "<h4>%s %s %s TDs: %s Receiving Yards: %s Rushing Yards: %s Passing Yards: %s Two point conversions: %s Fumbles Lost: %s Interceptions: %s Total Fantasy Points: %s </h4>" % (player_stats.team, player_stats.guess_position, player_stats.name, player_stats.tds, player_stats.receiving_yds, player_stats.rushing_yds, player_stats.passing_yds, player_stats.twoptm, player_stats.fumbles_lost, player_stats.passing_ints, playerPoints  )
+
+def calcFant(player):
+	fpoints = 0
+	tdpoints = 0
+	
+	if player.guess_position == "QB":
+		tdpoints = player.tds * 4
+	else:
+		tdpoints = player.tds * 6
+		
+	fpoints = tdpoints + (player.rushing_yds/10.0) + (player.receiving_yds/10.0) + (player.passing_yds/25.0) + (player.twoptm * 2) - (player.passing_ints * 2) - (player.fumbles_lost * 2)
+	return fpoints
+
+def playerOutput(playersList):
+	for p in playersList:
+		stats = players.name(p)
+		pPoints = calcFant(stats)
+		print "<h4>%s %s %s TDs: %s Receiving Yards: %s Rushing Yards: %s Passing Yards: %s Two point conversions: %s Fumbles Lost: %s Interceptions: %s Total Fantasy Points: %s </h4>" % (stats.team,  stats.guess_position, stats.name, stats.tds, stats.receiving_yds, stats.rushing_yds, stats.passing_yds, stats.twoptm, stats.fumbles_lost, stats.passing_ints, pPoints)
+		
+playerOutput(playersArr)
+
 print "</html>"
+
+
+	
+	
 
 
 	
